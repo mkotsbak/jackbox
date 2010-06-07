8342d89f0e5c323cb5aed6b866a56e8284811be3
Marius B. Kotsbak <marius@boostcom.no>
2010-06-07 18:44:13 +0200

    Added test using PowerMock.
    It seems to be able to do what we need for generated test from captured method calls data!
    
    But it fails when running through maven, because the aspects are interacting... Maybe split this into a seaparate project?

--- src/test/java/no/muda/jackbox/persistance/mocking/MockingLibTest.java
+++ src/test/java/no/muda/jackbox/persistance/mocking/MockingLibTest.java
@@ -1,22 +1,57 @@
 package no.muda.jackbox.persistance.mocking;
 
+import static org.easymock.EasyMock.expect;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.junit.Assert.fail;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.expectNew;
 import no.muda.jackbox.example.ExampleDependency;
 import no.muda.jackbox.example.ExampleRecordedObject;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+//@PrepareForTest( {ExampleRecordedObject.class, MockingLibTest.class})
+@PrepareForTest( {ExampleRecordedObject.class} )
 public class MockingLibTest {
     public static final String testArgument1 = "testargument1";
     public static final String testMockedReturnValue_ExampleDependency_invokedMethodOnDependency = "mockedreturn1";
-    public static final int testMockedReturnValue_ExampleDependency_exampleMethod = 435432;
+    public static final int testMockedReturnValue_ExampleRecordedObject_exampleMethod = 435432;
     private static final int testAdd1 = 2;
     private static final int testAdd2 = 5;
 
     @Test
-    public void testPowerMock() {
-        
+    public void testPowerMockDirectCall() throws Exception {
+        ExampleRecordedObject eroMock = createMock(ExampleRecordedObject.class);
+        expectNew(ExampleRecordedObject.class).andReturn(eroMock);
+//          expectNew(ExampleRecordedObject.class).andReturn(new ExampleRecordedObject());
+        expect(eroMock.exampleMethod(testAdd1, testAdd2)).andReturn(testMockedReturnValue_ExampleRecordedObject_exampleMethod);
+
+        //replay(eroMock, ExampleRecordedObject.class);
+        PowerMock.replayAll();
+
+        testExampleMethod();
+
+        PowerMock.verifyAll();
+    }
+
+    @Test
+    public void testPowerMockIndirectCall() throws Exception {
+        ExampleDependency edMock = createMock(ExampleDependency.class);
+
+        expectNew(ExampleDependency.class).andReturn(edMock);
+        expect(edMock.invokedMethodOnDependency(testArgument1)).andReturn(testMockedReturnValue_ExampleDependency_invokedMethodOnDependency);
+
+        //replay(eroMock, ExampleRecordedObject.class);
+        PowerMock.replayAll();
+
+        testExampleMethodThatDelegatesToDependency();
+
+        PowerMock.verifyAll();
     }
 
     @Test
@@ -58,6 +93,6 @@ public class MockingLibTest {
         ExampleRecordedObject obj = new ExampleRecordedObject();
         int result = obj.exampleMethod(testAdd1, testAdd2);
         assertThat(result).isNotEqualTo(testAdd1 + testAdd2);
-        assertThat(result).isEqualTo(testMockedReturnValue_ExampleDependency_exampleMethod);
+        assertThat(result).isEqualTo(testMockedReturnValue_ExampleRecordedObject_exampleMethod);
     }
 }
